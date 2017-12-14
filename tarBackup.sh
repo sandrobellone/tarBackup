f58fc1547e19aac520f23e6617423948f748c970
Sandro Bellone <asp20@libero.it>
2017-12-14 21:11:56 +0100

    risolti bug
    -Risolti bug
    -diminuita lunghezza nomi

--- tarBackup.sh
+++ tarBackup.sh
@@ -3,7 +3,7 @@
 #versione 0.10 del 02 Ottobre 2017
 #Copyright 2017 Sandro Bellone
 #Rilasciato secondo i termini della Licenza Pubblica Generica GNU
-#Esegue il backup di $ORIGINE in $DESTINAZIONE
+#Esegue il backup di $ORIG in $DEST
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -19,26 +19,29 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 T_INIZIO=`date +%s%N`
-NOME_BACKUP=prove2
-ORIGINE=/home/sandro
-DESTINAZIONE=/home/sandro/backup
-NUMERO_MAX_BACKUP_COMPLETI=3
-NUMERO_MAX_BACKUP_INCREMENTALI=5
-CARTELLA_CORRENTE="${0%/*}" #parameter expansion
+NOME_BACK=prove2
+ORIG=/home/sandro
+DEST=/home/sandro/backup
+NMAX_BACK_COMPL=3
+NMAX_BACK_INCR=5
+HOME="${0%/*}" #parameter expansion
 DEBUG=0
 LOGGING=0
 
 DATA=`date +%y%m%d_%H%M%S`
-NOME_FILE_INC=$DESTINAZIONE"/"$NOME_BACKUP"_"$DATA"_inc.tar.gz"
-NOME_FILE_COMPL=$DESTINAZIONE"/"$NOME_BACKUP"_"$DATA"_full.tar.gz"
-NOME_FILE_LISTA=$DESTINAZIONE"/"$NOME_BACKUP"_"$DATA"_list".idx
-NOME_ULTIMO_FILE_LISTA=$DESTINAZIONE"/"$NOME_BACKUP"_list".idx
+NOME_FILE_INC=$DEST"/"$NOME_BACK"_"$DATA"_inc.tar.gz"
+NOME_FILE_COMPL=$DEST"/"$NOME_BACK"_"$DATA"_full.tar.gz"
+NOME_FILE_LISTA=$DEST"/"$NOME_BACK"_"$DATA"_list".idx
+NOME_ULTIMO_FILE_LISTA=$DEST"/"$NOME_BACK"_list".idx
 
 
 #Funzione stampa
 function _stampa
 {
-  echo $@ | tee -a $CARTELLA_CORRENTE/tarBackup_log.txt
+  if [ $LOGGING -eq 1 ]; then
+    echo $@ | tee -a $HOME/tarBackup_log.txt
+  else echo $@
+  fi
 }
 
 #Funzione uscita
@@ -46,7 +49,8 @@ function _esci
 {
   T_FINE=`date +%s%N`
   T_DIFF=$((T_FINE - T_INIZIO))
-  _stampa "Tempo impiegato: $((T_DIFF/1000000000))sec$((T_DIFF/1000000%1000))msec"
+  _stampa "Tempo impiegato: $((T_DIFF/1000000000))sec\
+           $((T_DIFF/1000000%1000))msec"
   if [ $DEBUG -eq 1 ]; then
     _stampa "-->"cod.uscita=$1
   fi
@@ -57,10 +61,10 @@ function _esci
 function _check_exist
 {
   _stampa -n "controllo esistenza cartelle  ... "
-  if [ ! -d $ORIGINE ]; then
+  if [ ! -d $ORIG ]; then
     _stampa 'origine non esiste'
     _esci 1
-  elif [ ! -d $DESTINAZIONE ]; then
+  elif [ ! -d $DEST ]; then
     _stampa 'destinazione non esiste'
     _esci 1
   fi
@@ -70,17 +74,17 @@ function _check_exist
 #Funzione backup, lancia il comando tar e fa una copia del file lista
 function _backup
 {
-  NOME_CARTELLA_ORIGINE=`echo $ORIGINE| tr '/' '\n'|tail -n1`
+  NOME_CARTELLA_ORIG=`echo $ORIG| tr '/' '\n'|tail -n1`
   if [ $DEBUG -eq 1 ]; then
-    _stampa -e '\n-->'/bin/tar -C $ORIGINE/..\
+    _stampa -e '\n-->'/bin/tar -C $ORIG/..\
            --listed-incremental=$NOME_FILE_LISTA\
-           -czf $1 $NOME_CARTELLA_ORIGINE\
-		   --exclude=$NOME_BACKUP*{_full,_inc,_list}*
+           -czf $1 $NOME_CARTELLA_ORIG\
+		   --exclude=$NOME_BACK*{_full,_inc,_list}*
   fi
-  /bin/tar -C $ORIGINE/..\
+  /bin/tar -C $ORIG/..\
            --listed-incremental=$NOME_FILE_LISTA\
-           -czf $1 $NOME_CARTELLA_ORIGINE\
-		   --exclude=$NOME_BACKUP*{_full,_inc,_list}*
+           -czf $1 $NOME_CARTELLA_ORIG\
+		   --exclude=$NOME_BACK*{_full,_inc,_list}*
   cp $NOME_FILE_LISTA $NOME_ULTIMO_FILE_LISTA
 }
 
@@ -98,7 +102,7 @@ function _completo
 while getopts "hdl" opt; do
   case $opt in
     h)
-	  cat $CARTELLA_CORRENTE/tarBackup_Readme.txt
+	  cat $HOME/tarBackup_Readme.txt
       _esci 0
       ;;
 	d)
@@ -108,8 +112,8 @@ while getopts "hdl" opt; do
 	l)
 	  echo "-->Logging attivato"
 	  LOGGING=1
-	  echo "---------------------------" >> $CARTELLA_CORRENTE/tarBackup_log.txt
-	  echo "-->" inizio Logging - `date` >> $CARTELLA_CORRENTE/tarBackup_log.txt
+	  echo "---------------------------" >> $HOME/tarBackup_log.txt
+	  echo "-->" inizio Logging - `date` >> $HOME/tarBackup_log.txt
 	  ;;
     \?)
       _stampa "Opzione non valida: -$OPTARG" >&2
@@ -117,39 +121,39 @@ while getopts "hdl" opt; do
       ;;
   esac
 done
-if [ $LOGGING -eq 0 ]; then $CARTELLA_CORRENTE/tarBackup_log.txt=/dev/null; fi
 _check_exist
 if [ $DEBUG -eq 1 ]; then
-  _stampa "-->CARTELLA_CORRENTE="$CARTELLA_CORRENTE
-  _stampa "-->NOME_BACKUP="$NOME_BACKUP
+  _stampa "-->HOME="$HOME
+  _stampa "-->NOME_BACK="$NOME_BACK
   _stampa "-->NOME_FILE_COMPL"=$NOME_FILE_COMPL
   _stampa "-->NOME_FILE_INC"=$NOME_FILE_INC
 fi
-NUMERO_BACKUP_COMPLETI=`ls -X1 $DESTINAZIONE/$NOME_BACKUP* 2> /dev/null |grep "_full."|wc -l`
-NUMERO_BACKUP_INC=`ls -rX1 $DESTINAZIONE/$NOME_BACKUP*{full,inc}* 2> /dev/null | grep -m 1 -n "_full."|cut -d':' -f1`
-if [ -z $NUMERO_BACKUP_INC ]; then
-   NUMERO_BACKUP_INC=0
+NBACK_COMPL=`ls -X1 $DEST/$NOME_BACK* 2> /dev/null |grep "_full."|wc -l`
+NBACK_INCR=`ls -rX1 $DEST/$NOME_BACK*{full,inc}* 2> /dev/null |\
+            grep -m 1 -n "_full."|cut -d':' -f1`
+if [ -z $NBACK_INCR ]; then
+   NBACK_INCR=0
 else
-  ((NUMERO_BACKUP_INC -= 1))
+  ((NBACK_INCR -= 1))
 fi
-_stampa Rilevati $NUMERO_BACKUP_COMPLETI backup completi e $NUMERO_BACKUP_INC incrementali
-if [ $NUMERO_BACKUP_COMPLETI -gt $NUMERO_MAX_BACKUP_COMPLETI ]; then
-  _stampa 'Troppi backup interi, procedo alla cancellazione dei vecchi backup?'
-  OLDEST=`ls -c1 $DESTINAZIONE/$NOME_BACKUP*{_full,_inc,_list}*|grep -m $NUMERO_MAX_BACKUP_COMPLETI "_full"|tail -n 1`
+_stampa Rilevati $NBACK_COMPL backup completi e $NBACK_INCR incrementali
+if [ $NBACK_COMPL -gt $NMAX_BACK_COMPL ]; then
+  _stampa 'Troppi backup interi, i vecchi backup saranno cancellati'
+  OLDEST=`ls -c1 $DEST/$NOME_BACK*{_full,_inc,_list}*|\
+          grep -m $NMAX_BACK_COMPL "_full"|tail -n 1`
   if [ $DEBUG -eq 1 ]; then echo OLDEST=$OLDEST; fi
-  for file in `ls -c1 $DESTINAZIONE/$NOME_BACKUP*{_full,_inc,_list}*`
+  for file in `ls -c1 $DEST/$NOME_BACK*{_full,_inc,_list}*`
   do
     if [[ $file < $OLDEST ]]; then
-	  _stampa cancello $file
+	  _stampa ELIMINO: $file
 	  rm $file
 	fi
   done
-  echo "-->"
 fi
-if [ $NUMERO_BACKUP_COMPLETI -eq 0 ]; then
+if [ $NBACK_COMPL -eq 0 ]; then
   _stampa -n 'Non ci sono backup interi, procedo ad un backup intero ... '
   _completo
-elif [ $NUMERO_BACKUP_INC -ge $NUMERO_MAX_BACKUP_INCREMENTALI ]; then
+elif [ $NBACK_INCR -ge $NMAX_BACK_INCR ]; then
   _stampa -n 'Troppi backup incrementali, procedo ad un backup intero ... '
   _completo
 else
